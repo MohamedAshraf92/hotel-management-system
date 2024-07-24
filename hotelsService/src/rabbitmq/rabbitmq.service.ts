e54d162b6a76b1cb830cb232c8a2fc40df051e4a
MohamedAshraf92 <eng.mohamed.hfs@gmail.com>
2024-07-24 09:55:18 +0300

    cleanup

--- hotelsService/src/rabbitmq/rabbitmq.service.ts
+++ hotelsService/src/rabbitmq/rabbitmq.service.ts
@@ -9,19 +9,3 @@ export class RabbitmqService {
     await this.amqpConnection.publish('PAYMENT_SERVICE', routingKey, message);
   }
 }
-// @Injectable()
-// export class RabbitMQService {
-//   constructor(
-//     @Inject('PAYMENT_SERVICE') private readonly RMQClient: ClientProxy,
-//   ) {}
-
-//   async publishMessage(pattern: string, message: any) {
-//     this.RMQClient.emit(pattern, message).subscribe();
-//     console.log('Message sent to RabbitMQ: new_payment');
-//   }
-
-//   @EventPattern('new_payment')
-//   handleNewPayment(data: Record<string, unknown>) {
-//     console.log('Message received from RabbitMQ: new_payment', data);
-//   }
-// }
--- paymentService/src/rabbitmq/rabbitmq.service.ts
+++ paymentService/src/rabbitmq/rabbitmq.service.ts
@@ -13,30 +13,3 @@ export class RabbitmqService {
     // Process the message here
   }
 }
-
-// @Injectable()
-// export class RabbitmqService {
-//   private readonly logger = new Logger(RabbitmqService.name);
-//   constructor(
-//     @Inject('PAYMENT_SERVICE') private readonly RMQClient: ClientProxy,
-//   ) {
-//     this.logger.log('Consumer service initialized');
-//     console.log('CONNECTED');
-//   }
-
-//   @MessagePattern('new_payment')
-//   public async getNotifications(
-//     @Payload() data: any,
-//     @Ctx() context: RmqContext,
-//   ) {
-//     console.log('Entered');
-//     this.logger.log(`Received message: ${JSON.stringify(data)}`);
-//     const channel = context.getChannelRef();
-//     const originalMsg = context.getMessage();
-
-//     console.log({ originalMsg });
-//     console.log({ data });
-
-//     channel.ack(originalMsg);
-//   }
-// }
